b673268a69e949026b9e7e493332b79b9b39ce28
AEchRod <85241651+AEchRod@users.noreply.github.com>
2022-07-28 09:27:30 +0100

    Revision to add book titles to exported file
    This revision adds book titles to the exported Word file as bold text using the add.run() method and having created a dictionary from both book 'id' and 'title'.

--- Readwise to Word API - Rev. 00.py
+++ Readwise to Word API - Rev. 00.py
@@ -1,17 +1,17 @@
 import requests
 from docx import Document
-#import datetime
+# import datetime
 
 document = Document()
 
-#getting books that were updated last week, if needed:
-#a_week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
+# getting books that were updated last week, if needed:
+# a_week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
 
 querystring_category = {
     "category": "books",
 }
 
-#To check if GET request works
+# To check if GET request works
 response_auth = requests.get(
     url="https://readwise.io/api/v2/auth/",
     headers={"Authorization": "Token xxx"}
@@ -23,36 +23,45 @@ response_books = requests.get(
     params=querystring_category
 )
 
-#this variable contains the whole booklist
+# this variable contains the whole booklist as a dictionary
 data_booklist = response_books.json()
 
-#this variable contains only the results from the booklist
-booklist_results = data_booklist["results"]
+# this variable contains only the results from the booklist, since data_booklist['results'] is a list of dictionaries.
+booklist_results = data_booklist['results']
 
-x = [book["id"] for book in booklist_results]
+# this variable creates a list of each of the book ids
+book_x = [book['id'] for book in booklist_results]
 
-for id in x:
+book_title = [book['title'] for book in booklist_results]
+
+# this creates a dictionary with both, the id and book title.
+id_title = dict(zip(book_x, book_title))
+
+# we unpack dictionary so we can get book highlights by book id and add titles to our file to be exported.
+for id, title in id_title.items():
+
     querystring_bookid = {
         "book_id": id,
     }
-    #this query string selects books individually
+    # this query string selects books individually
 
-
-    #this requests gets the highlist list
+    # this requests gets the highlist list
     response = requests.get(
             url="https://readwise.io/api/v2/highlights/",
             headers={"Authorization": "Token xxx"},
             params=querystring_bookid
         )
 
-    #this retrieves a json from the highlist list
+    # this retrieves a json from the highlist list
     data_highlights = response.json()
 
-    highlight_results = data_highlights["results"]
+    # this variable contains only the results from the booklist, data_highlight['results'] is a list of dictionaries
+    highlight_results = data_highlights['results']
+
+    # this makes the book titles bold by using the add.run() method.
+    para = document.add_paragraph()
+    bold_para = para.add_run(title).bold=True
 
     for highlight in highlight_results:
-        document.add_paragraph(highlight["text"])
+        document.add_paragraph(highlight['text'])
     document.save('Highlights.docx')
-
-
-
